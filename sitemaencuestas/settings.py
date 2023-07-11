524d25d623d87b2c8634576e35f0802d7555c69f
Esteban AA <62820991+eathekilla@users.noreply.github.com>
2023-07-11 08:20:05 -0500

    clear staticfiles

--- sitemaencuestas/settings.py
+++ sitemaencuestas/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Encuesta.apps.EncuestaConfig',
-    'whitenoise.runserver_nostatic',
     'django_tables2',
     'bootstrap4',
 
@@ -127,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 #sprint(BASE_DIR)
